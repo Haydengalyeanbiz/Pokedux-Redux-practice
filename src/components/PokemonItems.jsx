12f9783747aa87fc81c8816617a8f559af3bf726
Hayden <haydengalyeanbiz@gmail.com>
2024-07-11 18:33:30 -0400

    finished phase 5

--- src/components/PokemonItems.jsx
+++ src/components/PokemonItems.jsx
@@ -1,44 +1,47 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getItems } from '../store/items';
 
 const PokemonItems = ({ pokemon, setEditItemId }) => {
-  const items = useSelector((state) => {
-    if (!pokemon.items) return null;
-    return pokemon.items.map(itemId => state.items[itemId]);
-  });
+	const dispatch = useDispatch();
+	const items = useSelector((state) => {
+		if (!pokemon.items) return null;
+		return pokemon.items.map((itemId) => state.items[itemId]);
+	});
+	console.log('THIS IS THE POKEMONID', pokemon.id);
 
-  if (!items) {
-    return null;
-  }
+	useEffect(() => {
+		dispatch(getItems(pokemon.id));
+	}, [dispatch, pokemon.id]);
 
-  return items.map((item) => (
-    <tr key={item.id}>
-      <td>
-        <img
-          className="item-image"
-          alt={item.imageUrl}
-          src={`${item.imageUrl}`}
-        />
-      </td>
-      <td>{item.name}</td>
-      <td className="centered">{item.happiness}</td>
-      <td className="centered">${item.price}</td>
-      {pokemon.captured && (
-        <td className="centered">
-          <button onClick={() => setEditItemId(item.id)}>
-            Edit
-          </button>
-        </td>
-      )}
-      {pokemon.captured && (
-        <td className="centered">
-          <button>
-            Delete
-          </button>
-        </td>
+	if (!items) {
+		return null;
+	}
 
-      )}
-    </tr>
-  ));
+	return items.map((item) => (
+		<tr key={item.id}>
+			<td>
+				<img
+					className='item-image'
+					alt={item.imageUrl}
+					src={`${item.imageUrl}`}
+				/>
+			</td>
+			<td>{item.name}</td>
+			<td className='centered'>{item.happiness}</td>
+			<td className='centered'>${item.price}</td>
+			{pokemon.captured && (
+				<td className='centered'>
+					<button onClick={() => setEditItemId(item.id)}>Edit</button>
+				</td>
+			)}
+			{pokemon.captured && (
+				<td className='centered'>
+					<button>Delete</button>
+				</td>
+			)}
+		</tr>
+	));
 };
 
 export default PokemonItems;
--- src/store/items.js
+++ src/store/items.js
@@ -1,58 +1,69 @@
-export const LOAD_ITEMS = "items/LOAD_ITEMS";
-export const UPDATE_ITEM = "items/UPDATE_ITEM";
-export const REMOVE_ITEM = "items/REMOVE_ITEM";
-export const ADD_ITEM = "items/ADD_ITEM";
+export const LOAD_ITEMS = 'items/LOAD_ITEMS';
+export const UPDATE_ITEM = 'items/UPDATE_ITEM';
+export const REMOVE_ITEM = 'items/REMOVE_ITEM';
+export const ADD_ITEM = 'items/ADD_ITEM';
 
 const load = (items, pokemonId) => ({
-  type: LOAD_ITEMS,
-  items,
-  pokemonId
+	type: LOAD_ITEMS,
+	items,
+	pokemonId,
 });
 
 const update = (item) => ({
-  type: UPDATE_ITEM,
-  item
+	type: UPDATE_ITEM,
+	item,
 });
 
 const add = (item) => ({
-  type: ADD_ITEM,
-  item
+	type: ADD_ITEM,
+	item,
 });
 
 const remove = (itemId, pokemonId) => ({
-  type: REMOVE_ITEM,
-  itemId,
-  pokemonId
+	type: REMOVE_ITEM,
+	itemId,
+	pokemonId,
 });
+
+export const getItems = (id) => async (dispatch) => {
+	console.log('HELLO?', id);
+	const response = await fetch(`/api/pokemon/${id}/items`);
+	// console.log('THIS IS THE RESPONSE', response);
+	if (response.ok) {
+		const allItems = await response.json();
+		dispatch(load(allItems, id));
+		return allItems;
+	}
+};
 
 const initialState = {};
 
 const itemsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOAD_ITEMS: {
-      const newItems = {};
-      action.items.forEach(item => {
-        newItems[item.id] = item;
-      })
-      return {
-        ...state,
-        ...newItems
-      }
-    }
-    case REMOVE_ITEM: {
-      const newState = { ...state };
-      delete newState[action.itemId];
-      return newState;
-    }
-    case ADD_ITEM:
-    case UPDATE_ITEM: 
-      return {
-        ...state,
-        [action.item.id]: action.item
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case LOAD_ITEMS: {
+			const newItems = {};
+			action.items.forEach((item) => {
+				newItems[item.id] = item;
+			});
+			return {
+				...state,
+				...newItems,
+			};
+		}
+		case REMOVE_ITEM: {
+			const newState = { ...state };
+			delete newState[action.itemId];
+			return newState;
+		}
+		case ADD_ITEM:
+		case UPDATE_ITEM:
+			return {
+				...state,
+				[action.item.id]: action.item,
+			};
+		default:
+			return state;
+	}
 };
 
 export default itemsReducer;
